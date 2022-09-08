ad9e4cf2d6b386eee1112b1116758d08ddabe3d7
StephenMukoma <mbuthastevemukoma@gmail.com>
2022-09-08 08:50:31 -0700

    Art is beautiful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:Always 1 (Success)
+ */
+int main(void)
+{
+	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", arr);
+	return (1);
+}

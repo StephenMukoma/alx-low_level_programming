1a69fb396ba431ffc9c262a0d74c1f63733dc470
StephenMukoma <mbuthastevemukoma@gmail.com>
2022-09-09 08:44:35 -0700

    6printnumber

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+int num;
+
+for (num = 48; num < 58; num++)
+{
+	putchar(num);
+}
+putchar('\n');
+return (0);
+}

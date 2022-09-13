155a5811ce0f34cb58db4ae06bba0070bbe2d1f9
StephenMukoma <mbuthastevemukoma@gmail.com>
2022-09-13 06:31:10 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always (0).
+ */
+int main(void)
+{
+	char _putchar;
+
+	 putchar(_putchar);
+putchar('\n');
+
+	return (0);
+}

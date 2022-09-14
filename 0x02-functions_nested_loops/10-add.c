58082ca325d375248806174f7851b1ecb9392df3
StephenMukoma <mbuthastevemukoma@gmail.com>
2022-09-14 09:43:40 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add 2 numbers
+ *
+ * @num1: parameter 1
+ *
+ * @num2: parameter 2
+ *
+ * Return: sum
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}

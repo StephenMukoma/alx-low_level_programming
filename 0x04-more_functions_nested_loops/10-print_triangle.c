520f4c9d2df89b84c1b0f0a985dace962394bde8
StephenMukoma <mbuthastevemukoma@gmail.com>
2022-09-15 10:39:13 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *print_triangle - print triangle
+ *
+ *@size: parameter size of triangle
+ *
+ */
+void print_triangle(int size)
+{
+	int count1, count2, count3;
+
+	if (size > 0)
+	{
+		for (count1 = 1; count1 <= size; count1++)
+		{
+			for (count2 = 1; count2 <= (size - count1); count2++)
+			{
+				_putchar(' ');
+			}
+			for (count3 = 0; count3 <= (size - count2); count3++)
+			{
+				_putchar('\n');
+			}
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+
+}

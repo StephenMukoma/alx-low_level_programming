7003f0bce7bd579af7549ac13094662b1ab19a1a
StephenMukoma <mbuthastevemukoma@gmail.com>
2022-09-15 09:59:15 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *fizz_buzz - print numbers and fizzbuzz
+ *
+ * @FB: parameter divisible 3,5
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	int FB;
+
+	for (FB = 1; FB <= 100; FB++)
+	{
+	if (FB % 3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (FB % 5 == 0)
+	{
+		printf("Buzz ");
+	}
+	else if (FB % 3 && FB % 5 == 0)
+	{
+		printf("Fizz Buzz ");
+	}
+	else
+	{
+		printf("%d ", FB);
+	}
+	}
+	printf("\n");
+	return (0);
+}

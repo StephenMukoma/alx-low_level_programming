703b1e3d14b63eb894081f6d34ef31e0804ae84c
StephenMukoma <mbuthastevemukoma@gmail.com>
2022-09-15 07:17:16 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print num
+ *
+ * Return: 0 Success
+ */
+void print_numbers(void)
+{
+	int num;
+
+		for (num = 48; num <= 57; num++)
+		{
+			_putchar(num);
+		}
+	_putchar('\n');
+
+}

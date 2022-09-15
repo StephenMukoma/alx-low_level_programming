cb57fef12cca9803931857d292ee50e8583103c6
StephenMukoma <mbuthastevemukoma@gmail.com>
2022-09-15 05:58:45 -0700

    Leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,18 +1,10 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
-
-
 #include <stdio.h>
-
-
 
 void positive_or_negative(i);
 int largest_number(int, int, int);
 void print_remaining_day(int, int);
 int convert_day(int, int);
-
-
-
 #endif /* MAIN_H*/

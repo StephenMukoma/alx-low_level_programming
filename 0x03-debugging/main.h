29fb98b5c73ee7e4651e50eac70245ecf662bed0
StephenMukoma <mbuthastevemukoma@gmail.com>
2022-09-15 06:07:57 -0700

    hot fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-void positive_or_negative(i);
+void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_day(int, int);
 int convert_day(int, int);

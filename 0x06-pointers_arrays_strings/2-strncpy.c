697ab1f79a6e2591c6cf8f2b4bfaea8e563491c4
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-09-22 14:59:42 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*_strncpy - copy string
+*
+*@dest: parameter string
+*
+*@src: parameter string
+*
+*@n: parameter string
+*
+*Return: Dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int s;
+
+	for (s = o; s < n && *(src + s) != '\0'; s++)
+	{
+		*(dest + s) = *(src + s);
+	}
+	while (s < n)
+	{
+		*(dest + s) = '\0';
+		s++
+	}
+	return (dest);
+}

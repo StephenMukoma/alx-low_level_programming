90661d750733e7e1a92d84bd6eec8d4d9ec517eb
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-09-22 16:04:20 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+*cap_string - Capitalizes string
+*
+*@str: parameter string
+*
+*Return: capitalized string
+*/
+char *cap_string(char *str)
+{
+	int c;
+
+	if (*str >= 'a' && *str <= 'z')
+	{
+		*str -= 32;
+	}
+	for (c = 1; *(str + c) != '\0'; c++)
+	{
+		if (*(str + (c - 1)) == ' ' ||
+		*(str + (c - 1)) == '\t' ||
+		*(str + (c - 1)) == '\n' ||
+		*(str + (c - 1)) == ',' ||
+		*(str + (c - 1)) == ';' ||
+		*(str + (c - 1)) == '.' ||
+		*(str + (c - 1)) == '!' ||
+		*(str + (c - 1)) == '?' ||
+		*(str + (c - 1)) == '"' ||
+		*(str + (c - 1)) == '(' ||
+		*(str + (c - 1)) == ')' ||
+		*(str + (c - 1)) == '{' ||
+		*(str + (c - 1)) == '}')
+		{
+			if (*(str + c) >= 'a' && *(str + c) <= 'z')
+			{
+				*(str + c) -= 32;
+			}
+		}
+		else
+		{
+		}
+
+	}
+}

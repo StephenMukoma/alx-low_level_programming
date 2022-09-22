18359133f3ecae202acadda304f66e2da84170aa
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-09-22 17:01:05 +0000

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*rot13 - encodes with rot13
+*
+*@str: parameter string
+*
+*Return: encode string
+*/
+char *rot13(char *str)
+{
+	int s, t;
+	char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (s = 0; *(str + s) != '\0'; s++)
+	{
+		for (t = 0; *(letters + t) != '\0'; t++)
+			{
+				if (*(str + s) == *(letters + t))
+				{
+					*(str + s) = *(rot13 + t);
+					break;
+				}
+			}
+	}
+	return (str);
+}
+

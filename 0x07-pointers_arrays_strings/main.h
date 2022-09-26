17aeb853dd51a5705462c08a0fcc6a36b3b63e62
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-09-26 18:10:23 +0000

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-_putchar.c
+int _putchar(char c);
 
 
 

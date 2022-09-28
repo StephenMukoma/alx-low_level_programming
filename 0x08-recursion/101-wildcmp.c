daff6e4441d046db2634e2093379399c6a908925
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-09-28 18:21:06 +0000

    8. Inception. Now, before you bother telling me it's impossible

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*wildcmp - Det if strings are similar
+*
+*@s1: string
+*@s2: string
+*
+*Return: 1 if similar else 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	{
+		return (0);
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+	return (0);
+}

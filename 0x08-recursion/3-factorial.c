0bb2dd0a6b74f37e6cd31ad20fbdb1a2c995208c
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-09-28 13:41:12 +0000

    3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*factorial - calculate factorial
+*
+*@n: integer
+*
+*Return: Factorial
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}

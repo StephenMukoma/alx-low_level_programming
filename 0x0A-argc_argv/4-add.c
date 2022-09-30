ecb81b0c75ba0862f73db9dfa41c7aafba2fd5c1
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-09-30 16:38:58 +0000

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+*main - prints sum
+*
+*@argc: argument
+*@argv: argument
+*
+*Return: 0 (success) else 1
+*/
+int main(int argc, char *argv[])
+{
+	int a, r, sum;
+
+	sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (r = 0; argv[a][r] != '\0'; r++)
+		{
+			if (!isdigit(argv[a][r]))
+			{
+				print("Error\n");
+				return (1);
+			}
+		}
+		total += atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}

0d38cb3c738e1b8dae08cab81316dcec6fd845ab
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-10-04 16:08:37 +0000

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_strdup -  returns a pointer to a newly allocated space in
+*memory, which contains a copy of the string given as a parameter.
+*
+*@str: pointer
+*
+*Return: pointer to copied string (success) else NULL
+*/
+char *_strdup(char *str)
+{
+	int length, m;
+	char *cc;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (length = 0; *(str + length) != '\0'; length++)
+	{
+	}
+	cc= malloc(sizeof(char) * (length + 1));
+
+	if (cc == NULL)
+	{
+		return (NULL);
+	}
+
+	for (m = 0; m < length; m++)
+	{
+		cc[m] = str[m];
+	}
+	return (cc);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
 
 
 

c78eb6d19d99b121e007d7725bd2349465c468f1
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-10-24 11:08:00 +0000

    error fix print_list function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+*print_list -  function that prints all the elements of a list
+*
+*@list_t:
+*@h:
+*
+*Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	
+}

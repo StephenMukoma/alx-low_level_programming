b84921cd6a71095b6f1670104075fd8d7afcbba7
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-10-24 18:44:34 +0000

    execute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+*before_main - function executed before main function
+*/
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

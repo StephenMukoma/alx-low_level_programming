019efeb2f7f856e66621a18d922bcd3f99518737
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-10-24 18:31:46 +0000

    function that frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+*free_list - function that frees a list
+*
+*@head:
+*
+*/
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}	
+}

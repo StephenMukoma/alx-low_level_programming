27be277cf095494963e5292c255a8f09324f0a6d
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-10-27 11:34:36 +0000

    feat, number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+*listint_len - function that returns the number of elements in a linked list
+*
+*@h: pointer to first element of list
+*
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *ptr;
+
+	ptr = h;
+
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
+

fd034180c92d2f8d57f1d5e82e41c9d37f054996
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-10-31 18:34:42 +0000

    feat, value of bit to zero at an index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*clear_bit - function that sets the value of a bit to 0 at a given index
+*
+*@n: pointer to value
+*
+*@index: index starting from 0 of the bit you want to set
+*
+*Return: 1 success else -1
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int num;
+
+	if (index > 63)
+		return (-1);
+
+	num = 1 << index;
+
+	if (*n & num)
+		*n ^= num;
+
+	return (1);
+}

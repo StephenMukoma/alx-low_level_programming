565d47c5b385a1e0e374fd1d5abb92ac688c5363
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-10-31 13:16:07 +0000

    fix bugs, binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,32 @@
 #include "main.h"
 
 /**
+*_pow - raises base to power
+*
+*@base: int to raise
+*@power: power to raise
+*
+*Return: Unsigned int raised to a power
+*/
+unsigned int _pow(unsigned int base, int power)
+{
+        unsigned int output = 1;
+
+        if (power == 0)
+        {
+                return (1);
+        }
+
+        while (power > 0)
+        {
+                output *= base;
+                power--;
+        }
+        return (output);
+}
+
+
+/**
 *binary_to_uint - converts a binary number to unsigned int
 *
 *@b: pointer to a string
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-unsigned int _pow(unsigned int base, int power);
 unsigned int binary_to_uint(const char *b);
 
 #endif

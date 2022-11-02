4e59a2ce6f9bed4a457d20bbccaac9acf7f09a19
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-11-02 13:01:41 +0000

    fix, betty coding style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
 *read_textfile - function that reads a text file and
 *prints it to the POSIX standard output
 *
-*@filename:
+*@filenamei: name of file being read
 *@letters: number of letters it could read and print
 *
 *Return: actual number of letters it could read and print

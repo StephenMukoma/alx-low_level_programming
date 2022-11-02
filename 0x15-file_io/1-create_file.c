542d8daca7aa8f0ef9f2e856ad48d7f451d26883
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-11-02 14:24:51 +0000

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,44 @@
 #include "main.h"
 
+/**
+*create_file - function that creates a file
+*
+*@filename: name of file to create
+*@text_content: content to write in file
+*
+*Return: Success (1)
+*/
+int create_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+	int c = 0;
 
+	if (filename == NULL)
+	{
+		return (-1);
+`	}
+
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+
+	fp = fopen(filename, "w");
+
+	if (fp == NULL)
+	{
+		return (-1);
+	}
+
+	chmod(filename, 0600);
+
+	while (text_content[c] != '\0')
+	{
+		putc(text_content[c], fp);
+		c++;
+	}
+
+	fclose(fp);
+	return (1);
+
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <unistd.h>
-
-
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
 
 #endif

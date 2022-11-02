27804695f729895fc71bee9c7ceb58b5bbb6e935
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-11-02 12:56:29 +0000

    feat, read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+*read_textfile - function that reads a text file and
+*prints it to the POSIX standard output
+*
+*@filename:
+*@letters: number of letters it could read and print
+*
+*Return: actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	int c;
+	size_t i = 0;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fp = fopen(filename, "r");
+
+	if (fp == NULL)
+	{
+		return (0);
+	}
+
+	c = fgetc(fp);
+
+	while (i < letters && c != EOF)
+	{
+		write(STDOUT_FILENO, &c, 1);
+		c = fgetc(fp);
+		i++;
+	}
+
+	fclose(fp);
+	fp = NULL;
+	return (i);
+
+
+}

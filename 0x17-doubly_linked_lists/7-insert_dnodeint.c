d0177d21fce036fcac3e1b57156da3d107fde640
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-08 17:52:13 +0000

    fix, requirement code fail

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
-*insert_dnodeint_at_index- 
+*insert_dnodeint_at_index- add new node
 *
 *@h: pointer to address of list
 *@idx: index of new node
@@ -11,13 +11,26 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h, *temp2, *newp;
-	unsigned int counter = idx;
+	unsigned int counter = idx, len = 0;
 
 	if (idx == 0)
 	{
 		newp = add_dnodeint(h, n);
 		return (newp);
 	}
+
+	while (temp)
+	{
+		temp = temp->next;
+		len++;
+	}
+
+	if (len == idx)
+	{
+		newp = add_dnodeint_end(h, n);
+		return (newp);
+	}
+	temp = *h;
 
 	while (counter != 1)
 	{
@@ -28,13 +41,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	newp = malloc(sizeof(dlistint_t));
 	newp->prev = NULL;
-	newp->n =n;
+	newp->n = n;
 	newp->next = NULL;
 
 	temp->next = newp;
 	temp2->prev = newp;
 	newp->prev = temp;
 	newp->next = temp2;
-	
+
 	return (newp);
 }

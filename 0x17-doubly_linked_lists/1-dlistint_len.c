df3c12aa1b44f84eb9186c7a6b16885717b7a866
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-08 11:05:14 +0000

    feat, number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*dlistint_len
+*
+*@h: pointer to list
+*
+*Return: number of elements in a list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}

473f22c2cec77362a4ca64534c8bfba238570eaa
Stephen Mukoma <mbuthastevemukoma@gmail.com>
2022-12-08 12:01:17 +0000

    feat, add new node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+*add_dnodeint - adds new node at beginning of list
+*
+*@head: pointer to first element on list
+*@n: data to be added
+*
+*Returns: address of new element
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	temp = malloc(sizeof(dlistint_t));
+
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+
+	temp->prev = NULL;
+	temp->n = n;
+	temp->next = NULL;
+	temp->next = *head;
+	*head = temp;
+
+	return (*head);
+}
